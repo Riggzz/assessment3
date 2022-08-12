4368b10854493f446370a1405044a5a308274126
= <=>
2022-08-12 12:40:13 -0600

    commit final

---
+++ assessment-front-end-web/neighborhood.js
@@ -0,0 +1,6 @@
+let rando = ["https://www.caferio.com/locations/heber", "https://www.facebook.com/people/Hub-Cafe/100033469586529/", "http://lolasstreetkitchen.com/", "https://www.back40utah.com/", "https://bettyscafeofutah.com/" ]
+
+let random = document.querySelector('#rester');
+randomRes.addEventListener('click', () => {
+	Math.random(rando)
+})
